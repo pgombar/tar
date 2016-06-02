0972b8ad9a51cd4dd47eb0b07ab5f4d8dc01bfcb
Ivan Katanić <ivan.katanic@gmail.com>
2016-06-02 14:57:14 +0200

    blabla

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 
 import utils
+import word2vec
+import numpy as np
 
 input_dir = '../data/trial-dataset/'
 output_file = 'main_ranking'
+simple_wiki_freqs_file = '../simple_wiki/freqs.txt'
 
 tasks = utils.parse_input_file(input_dir)
+
 
 def rank_stupid(s):
     ret = []
@@ -13,7 +17,38 @@ def rank_stupid(s):
         ret.append(sorted(subs, key=len))
     return ret
 
-rankings = rank_stupid(tasks)
 
-utils.output(output_file, rankings)
+def rank_by_freqs(s, freqs):
+    def f(word):
+        if word in freqs:
+            return -freqs[word]
+        return 0
     
+    ret = []
+    for _, subs in s:
+        ret.append(sorted(subs, key=f))
+    return ret
+
+
+def rank_by_context_similarity(s):
+    model = word2vec.load('../simple_wiki/dump.bin')
+    ret = []
+    for (sentence, idx), subs in s:
+        word = sentence[idx]
+        def score(sub):
+            ret_score = 0
+            for i, w in enumerate(sentence):
+                if i != idx:
+                    ret_score += utils.word2vec_similarity(model, w, sub)
+            return -ret_score
+        bla = map(lambda sub: (score(sub), sub), subs)
+        bla = sorted(bla)
+        ret.append(map(lambda (_, a): a, bla))
+        ret.append(sorted(subs, key=score))
+        print 'done'
+    return ret
+    
+simple_wiki_freqs = utils.read_freqs_file(simple_wiki_freqs_file)
+rankings = rank_by_context_similarity(tasks)
+utils.output(output_file, rankings)
+
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python
 
+import nltk
+from nltk import WordNetLemmatizer
+from nltk.corpus import stopwords
 import xml.etree.ElementTree as ET
+import numpy as np
+
+def parse_text(text):
+    """ 
+    1. tokenize
+    2. convert to lowercase
+    3. remove stopwords
+    4. return list of words
+    """
+
+    stops = stopwords.words('english')
+    tokens = nltk.word_tokenize(text)
+    tokens = map(lambda f: f.lower(), tokens)
+    tokens = filter(lambda f: f not in stops, tokens)
+    return tokens
 
 
 def parse_input_file(input_dir):
@@ -10,25 +28,29 @@ def parse_input_file(input_dir):
     
     Returns list of tasks in form of pairs (sentence, list of possible subs).
     Each sentence is a pair (list of strings, 0-based index of a string to be replaced).
-
-    e.g. of a task:
-    ((['That boy is', 'bright', 'and young'], 1), ['intelligent', 'bright', 'clever', 'smart'])
     """
 
     tree = ET.parse(input_dir + 'contexts.xml')
     root = tree.getroot()
-
+    
     ret = []
     for x in root.iter(tag='instance'):
         text = [c.strip() for c in x.itertext() if c.strip()]
         
         if len(text) == 3:
-            ret.append((text, 1))
+            pre = parse_text(text[0])
+            sub = [' '.join(parse_text(text[1]))]
+            suf = parse_text(text[2])
+            ret.append((pre + sub + suf, len(pre)))
         else:
-            if len(ret[0]) < len(ret[1]):
-                ret.append((text, 0))
+            if len(text[0]) < len(text[1]):
+                sub = [' '.join(parse_text(text[0]))]
+                suf = parse_text(text[1])
+                ret.append((sub + suf, 0))
             else:
-                ret.append((text, 1))
+                pre = parse_text(text[0])
+                sub = [' '.join(parse_text(text[1]))]
+                ret.append((pre + sub, len(pre)))
 
     subs = open(input_dir + 'substitutions').read()
     subs = subs.split('\n')
@@ -44,9 +66,48 @@ def parse_input_file(input_dir):
 
 def output(filepath, rankings):
     """ Writes rankings to file """
-
+    
     f = open(filepath, 'w')
     for idx, ranking in enumerate(rankings):
         out = map(lambda x: '{' + x + '}', ranking)
         out = ' '.join(out)
         f.write('Sentence {} rankings: {}\n'.format(idx + 1, out))
+
+
+def read_freqs_file(filepath):
+    """ Read word frequencies file and returns dictionary
+    where words are keys and frequencies are values.
+    """
+    f = open(filepath, 'r')
+    ret = {}
+    for line in f.read().split('\n'):
+        tokens = line.split(' ')
+        if len(tokens) == 2:
+            ret[tokens[0]] = int(tokens[1])
+    return ret
+    
+
+def lemmatizate_freqs_file(inp_file, out_file):
+    lemmatizater = WordNetLemmatizer()
+    freqs = read_freqs_file(inp_file)
+
+    nfreqs = {}
+    for word in freqs:
+        nword = lemmatizater.lemmatize(word)
+        if nword not in nfreqs:
+            nfreqs[nword] = 0
+        nfreqs[nword] += freqs[word]
+
+    f = open(out_file, 'w')
+    for word in sorted(nfreqs.keys()):
+        f.write('{} {}\n'.format(word, nfreqs[word]))
+    
+
+def word2vec_similarity(model, a, b):
+    if a not in model.vocab:
+        return 0
+    if b not in model.vocab:
+        return 0
+    va = model.get_vector(a)
+    vb = model.get_vector(b)
+    return np.dot(va, vb)
