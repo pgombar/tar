5eb0dd9bd16dfbfa41c6fc520b34efbfbf58ec56
Paula Gombar <gombarica@gmail.com>
2016-06-03 14:27:09 +0200

    add wiki freqs, svm output format util, svm_rank modules

--- src/main.py
+++ src/main.py
@@ -8,14 +8,38 @@ from scorer import Scorer
 input_dir = '../data/trial-dataset/'
 output_file = 'main_ranking'
 simple_wiki_freqs_file = '../simple_wiki/freqs.txt'
+wiki_freqs_file = '../wiki/freqs.txt'
 
 tasks = utils.parse_input_file(input_dir)
+
 
 class ScorerInvLength(Scorer):
     def score(self, _, sub):
         return 1.0 / len(sub)
 
+
+class ScorerCorpusComplexity(Scorer):
+    sw_freqs = {}
+    w_freqs = {}
+    def __init__(self):
+        self.sw_freqs = utils.read_freqs_file(simple_wiki_freqs_file)
+        self.w_freqs = utils.read_freqs_file(wiki_freqs_file, separator='\t')
+    def score(self, _, sub):
+        if sub in self.w_freqs and sub in self.sw_freqs:
+            return 1.0 / (self.w_freqs[sub] / (self.sw_freqs[sub] * 1.0))
+        return 0
+
+
+class ScorerWFreqs(Scorer):
+    freqs = {}
+    def __init__(self):
+        self.freqs = utils.read_freqs_file(wiki_freqs_file, separator='\t')
+    def score(self, _, sub):
+        if sub in self.freqs:
+            return self.freqs[sub]
+        return 0        
     
+
 class ScorerSWFreqs(Scorer):
     freqs = {}
     def __init__(self):
@@ -43,9 +67,22 @@ class ScorerContextSimilarity(Scorer):
     def score(self, (sentence, idx), sub):
         ret = 0
         for i, w in enumerate(sentence):
-            if i != idx:
-                ret += np.dot(self.vector(w), self.vector(sub))
+#            if i != idx:
+#                ret += np.dot(self.vector(w), self.vector(sub))
+            ret += np.dot(self.vector(w), self.vector(sub))
         return ret
+
+
+class ScorerBiran(Scorer):
+    s1 = 0
+    s2 = 0
+    def __init__(self):
+        self.s1 = ScorerCorpusComplexity()
+        self.s2 = ScorerInvLength()
+
+    def score(self, (sentence, idx), sub):
+        return self.s1.score((sentence, idx), sub) * self.s2.score((sentence, idx), sub)
+
 
 class ScorerComb(Scorer):
     s1 = 0
@@ -57,7 +94,8 @@ class ScorerComb(Scorer):
     def score(self, (sentence, idx), sub):
         return self.s1.score((sentence, idx), sub) * self.s2.score((sentence, idx), sub)
 
-scorer = ScorerComb()
+
+scorer = ScorerContextSimilarity()
 rankings = utils.rank_everything(scorer, tasks)
 utils.output(output_file, rankings)
 
--- src/utils.py
+++ src/utils.py
@@ -76,14 +76,14 @@ def output(filepath, rankings):
         f.write('Sentence {} rankings: {}\n'.format(idx + 1, out))
 
 
-def read_freqs_file(filepath):
+def read_freqs_file(filepath, separator=' '):
     """ Read word frequencies file and returns dictionary
     where words are keys and frequencies are values.
     """
     f = open(filepath, 'r')
     ret = {}
     for line in f.read().split('\n'):
-        tokens = line.split(' ')
+        tokens = line.split(separator)
         if len(tokens) == 2:
             ret[tokens[0]] = int(tokens[1])
     return ret
@@ -107,3 +107,66 @@ def lemmatizate_freqs_file(inp_file, out_file):
 
 def rank_everything(scorer, tasks):
     return map(lambda ((a, b), c): scorer.rank((a, b), c), tasks)
+    
+
+def get_svm_line(gold, qid, features):
+    """ Produce a string of the following SVM input format:
+        3 qid:1 1:1 2:1 3:0 4:0.2 5:0 # 1A
+    """
+    features = [1, 2, 3]
+    s = str(gold) + " qid:" + str(qid) + " "
+    for i,f in enumerate(features):
+        s += str(i+1) + ":" + str(f) + " "
+    s += "# " + str(qid) + "\n"
+    return s
+
+
+def get_sentence_data(gold_subs, subs):
+    """ Retrieve qid (sentence number) and the target value (gold for each sub).
+        Gold is a string in the format of:
+        "Sentence 1 rankings: {intelligent} {clever} {smart} {bright}",
+        subs is a list of possible substitutions.
+        Returns a list of (gold, qid) for each substitution.
+    """
+    ret = []
+    qid = gold_subs.split()[1]
+    gold_rank = map(lambda f: f.strip(), gold_subs.replace('}','').split('{')[1:])
+    for sub in subs:
+        # TODO skuziti kako parsirati {rijec, rijec2} jer ovdje nastaje problem
+        try:
+            gold = gold_rank.index(sub) + 1
+        except ValueError:
+            gold = -1
+        ret.append((gold, qid))
+    return ret
+
+
+def output_svm_file(filepath, gold_subs_file, subs_file, features):
+    """ Read gold rankings file and substitutions, parse them and
+        output a file in SVM format.
+        Features is a list of floats, filepath is the output file.
+    """
+    f = open(filepath, 'w')
+    
+    gold_subs_list = open(gold_subs_file).read()
+    gold_subs_list = gold_subs_list.split('\n')
+    if gold_subs_list[-1] == '':
+        gold_subs_list.pop()
+    
+    subs_list = open(subs_file).read()
+    subs_list = subs_list.split('\n')
+    if subs_list[-1] == '':
+        subs_list.pop()
+
+    assert len(subs_list) == len(gold_subs_list)
+    assert all([x[-1] == ';' for x in subs_list])
+    
+    subs_list = map(lambda x: x.split(';')[:-1], subs_list)
+    
+    for gold, sub in zip(gold_subs_list, subs_list):
+        sentence_data = get_sentence_data(gold, sub)
+        for gold, qid in sentence_data:
+            # TODO promijeniti u features[i]
+            f.write(get_svm_line(gold, qid, features))
+    f.close()
+
