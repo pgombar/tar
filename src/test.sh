8af6d537d420bd60de457fd46dbd7395e4bc71f7
Ivan Katanić <ivan.katanic@gmail.com>
2016-06-07 20:48:49 +0200

    scripts for train and testing

--- src/test.sh
+++ src/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-./extract_features.py ../data/trial-dataset/ train.dat
-../svm_rank_linux64/svm_rank_learn -c 200 -t 2 train.dat model.dat
-../svm_rank_linux64/svm_rank_classify train.dat model.dat predictions
-./recover_rankings.py ../data/trial-dataset/ predictions rankings
-./test-script/rank-scorer.py -i rankings -g ../data/trial-dataset/substitutions.gold-rankings
+./extract_features.py ../data/test-data/ test.dat
+../svm_rank_linux64/svm_rank_classify test.dat model.dat predictions_test
+./recover_rankings.py ../data/test-data/ predictions_test rankings_test
+./test-script/rank-scorer.py -i rankings_test -g ../data/test-data/substitutions.gold-rankings
---
+++ src/train.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./extract_features.py ../data/trial-dataset/ train.dat
+../svm_rank_linux64/svm_rank_learn -c 1   train.dat model.dat
+../svm_rank_linux64/svm_rank_classify train.dat model.dat predictions
+./recover_rankings.py ../data/trial-dataset/ predictions rankings
+./test-script/rank-scorer.py -i rankings -g ../data/trial-dataset/substitutions.gold-rankings
